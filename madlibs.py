634f5c9b7dce3b760b496d16cf3dbfdcc9265e30
Anna Laffoon <ree11194@gmail.com>
2022-07-21 11:43:49 -0400

    simple python madlib

---
+++ madlibs.py
@@ -0,0 +1,39 @@
+# #types ofstring concatenation
+# #subscribe to ___
+# youtuber = ""
+# print("subscribe to " + youtuber)
+# print("subscribe to {} ".format(youtuber))
+# #f string
+# print(f"subscribe to {youtuber}")
+
+name = input("Name: ")
+age = input("Age: ")
+noun1 = input("Noun: ")
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+noun2 = input("Noun: ")
+num = input("Number: ")
+noun3 = input("Noun: ")
+adj3 = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+adj4 = input("Adjective: ")
+verb3 = input("Verb: ")
+noun4 = input("Noun: ")
+adverb = input("Adverb: ")
+
+#American Psycho opening monologue
+
+madlib = f"I live in the American Gardens Building on West 81st Street on the 11th floor. My name is {name}. \
+I'm {age} years old. I believe in taking care of myself, and a balanced {noun1} and a {adj1} exercise routine. \
+In the morning, if my face is a little {adj2}, I'll put on an ice pack while doing my {noun2} crunches. \
+I can do {num} now. After I remove the ice pack I use a deep pore cleanser lotion. \
+In the shower I use a {noun3} activated gel cleanser, then a honey almond body scrub, and on the face a(n) {adj3} gel scrub. \
+Then I apply an herb-mint facial masque which I leave on for 10 minutes while I {verb1} the rest of my routine. \
+I always {verb2} an after shave lotion with little or no alcohol, because alcohol dries your face out and makes you look older. \
+Then moisturizer, then an anti-aging eye balm followed by a final moisturizing protective lotion. \
+There is an idea of a {name}. Some kind of abstraction. But there is no real me. Only an entity. \
+Something illusory. And though I can hide my {adj4} gaze, and you can {verb3} my hand and feel {noun4} gripping yours, \
+and maybe you can even sense our lifestyles are probably comparable, I {adverb} am not there."
+
+print(madlib)
